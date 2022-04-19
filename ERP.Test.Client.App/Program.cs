9b6c32acb14b5d6ca39d3508dd7bf189cfb1acc0
Nico Volling <nico.volling.2000@gmail.com>
2022-04-19 17:17:33 +0200

    Entfernen der GUI

--- ERP.Test.Client.App/Program.cs
+++ ERP.Test.Client.App/Program.cs
@@ -1,12 +1,7 @@
 ﻿using ERP.BaseLib.Output;
-using ERP.Client.WindowsForms;
-using ERP.Client.WindowsForms.Controls.BindableControls;
-using ERP.Client.WindowsForms.Controls.Windows;
 using ERP.Commands.Base;
-using ERP.Test.Client.Library.GUI;
 using ERP.Test.ObjectClients;
 using ERP.Test.Public.Library.Objects;
-using System.Windows.Forms;
 
 namespace ERP.Test.Client.App
 {
@@ -33,40 +28,14 @@ namespace ERP.Test.Client.App
                     Log.WriteLine(person1, ConsoleColor.Magenta);
 
                     Log.WriteLine("Test was successful", ConsoleColor.Magenta);
+
+                    Console.ReadLine();
                 }
             }
             catch
             {
                 throw;
             }
-
-            Dictionary<string, Dictionary<string, Func<BaseWindow>>> WindowList = new()
-            {
-                {
-                    "Menschen",
-                    new Dictionary<string, Func<BaseWindow>>()
-                    {
-                        { "Personen", () =>  new BaseWindow(new CP_Personen()) { Text = "Personen", Icon = ERP.Client.WindowsForms.Base.Resources.Icon, CanMaximize = false, CanResize = false } }
-                    }
-                },
-                {
-                    "GUI",
-                    new Dictionary<string, Func<BaseWindow>>()
-                    {
-                        { "Selection", () =>
-                        {
-                            BaseWindow BW = new BaseWindow(new B_BO_Selection_ContentPanel()) { Text = "Selection", Icon = ERP.Client.WindowsForms.Base.Resources.Icon, CanMaximize = true, CanResize = true };
-                            ((B_BO_Selection_ContentPanel)BW.ContentPanel).SetBusinessObjectList(new PersonClient(), new PersonClient().GetList());
-                            return BW;
-                        }
-                        }
-                    }
-                }
-            };
-
-            BaseForm BF;
-            BF = new BaseForm("ERP-Test", ERP.Client.WindowsForms.Base.Resources.WindowIcon, WindowList);
-            Application.Run(BF);
         }
     }
 }
